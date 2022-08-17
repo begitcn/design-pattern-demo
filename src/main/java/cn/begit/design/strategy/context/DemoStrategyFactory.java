bc65055d00699f87ca1aeb04b956a5c3cf8e0eba
guochao <begitcn@gmail.com>
2022-08-17 12:05:29 +0800

    优化处理策略模式示例代码

--- src/main/java/cn/begit/design/strategy/context/DemoStrategyFactory.java
+++ src/main/java/cn/begit/design/strategy/context/DemoStrategyFactory.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 /**
- * 策略接口核心调度工厂
+ * 策略接口核心调度工厂（或者context核心调度类）
+ * 可能说是工厂不很准确（工厂模式只关注产出对象，不由用户选择。而策略模式是关注行为，具体怎么处理怎么干由用户选择）
  * <br>
  * 根据 getResource(xxx) 方法传入的xxx调取对应 @Service(xxx) 实现类
  * <br>
+ * <p>
  * 具体使用详见controller
  *
  * @author guochao
